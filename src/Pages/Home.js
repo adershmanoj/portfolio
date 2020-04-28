928b07da8e9dbe41b4c9c61bee03cc142977b979
adershmanoj <adershmanoj@gmail.com>
2020-04-28 19:55:34 +0530

    Created hello world card in homepage

---
+++ src/Pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div className='container'>
+           <div className='left'>
+               <div className = 'helloworld'>
+                   <div className = "helloworld__text">
+                   <p>Hello,<br/>World!</p>
+                   <p>I'm <strong>Adersh</strong></p>
+                   </div>
+               </div>
+            </div> 
+        </div>
+    )
+}
+export default Home
