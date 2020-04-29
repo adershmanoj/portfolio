cf4e357d4c526d8b578f46bb7456d40bc72b0e01
adershmanoj <adershmanoj@gmail.com>
2020-04-29 11:02:00 +0530

    Added background and flexbox

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,14 +3,18 @@ import React from 'react';
 function Home() {
     return (
         <div className='container'>
-           <div className='left'>
-               <div className = 'helloworld'>
+            <div id='stars'></div>
+            <div className='left'>
+                <div className = 'helloworld'>
                    <div className = "helloworld__text">
                    <p>Hello,<br/>World!</p>
                    <p>I'm <strong>Adersh</strong></p>
                    </div>
-               </div>
+                </div>
             </div> 
+            <div className='right'>
+                <h1>Right</h1>
+            </div>
         </div>
     )
 }
