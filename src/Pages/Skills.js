0a5bf913dc9231253d558c64582e56341aa999dd
adershmanoj <adershmanoj@gmail.com>
2020-05-18 11:53:39 +0530

    Added icons for skills

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -1,33 +1,49 @@
 import React, { memo, useState } from 'react';
 
+const skills = [
+    {
+        name: 'ReactJs',
+        category: 'Web Development',
+        logo: 'react.png'
+    },
+    {
+        name: 'JavaScript',
+        category: 'Web Development',
+        logo: 'javascript.png'
+    },
+    {
+        name: 'Java',
+        category: 'Backend',
+        logo: 'java.png'
+    },
+    {
+        name: 'Spring',
+        category: 'Backend',
+        logo: 'spring.png'
+    },
+    {
+        name: 'Python',
+        category: 'Backend / Scripting',
+        logo: 'python.png'
+    }
+]
+
 function Skills() {
     return (
         <div className='skills'>
             <div className='skills--header'>Skills</div>
             <div className='skills--container'>
-                <div className='skill'>
-                    <div className='skill--logo'><img src='javascript.png' alt='javascript' /></div>
-                    <div className='skill--info'>
-                        <h4>Web Development</h4>
-                        <h3>Javascript</h3>
+                {skills.map(skill => (
+                    <div className='skill'>
+                        <div className='skill--logo'><img src={'./icons/' + skill.logo} alt={skill.name} /></div>
+                        <div className='skill--info'>
+                            <h4>{skill.category}</h4>
+                            <h3>{skill.name}</h3>
+                        </div>
                     </div>
-                </div>
-                <div className='skill'>
-                    <div className='skill--logo'><img src='javascript.png' alt='javascript' /></div>
-                    <div className='skill--info'>
-                        <h4>Web Development</h4>
-                        <h3>Javascript</h3>
-                    </div>
-                </div>
-                <div className='skill'>
-                    <div className='skill--logo'><img src='javascript.png' alt='javascript' /></div>
-                    <div className='skill--info'>
-                        <h4>Web Development</h4>
-                        <h3>Javascript</h3>
-                    </div>
-                </div>
-                </div>
+                ))}
             </div>
+        </div>
     )
 }
 
