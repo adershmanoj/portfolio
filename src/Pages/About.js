dace62a9914dada93e19e8d8583e607f718dcb8f
adershmanoj <adershmanoj@gmail.com>
2020-05-22 10:23:50 +0530

    Skills page and overall styling

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,20 +1,15 @@
-import React, { memo, useState } from 'react';
-import FadeIn from 'react-fade-in';
+import React, { memo } from 'react';
 
 import InfoPane from '../Components/InfoPane';
 import Header from '../Components/Header';
-import Typewriter from '../Components/Typewriter';
 
-const languageText = ["I am fluent in English.", "നാട്ടിൽ എവിടെയാ ?", "मैं कठिनाई से हिंदी बोल सकता हूं.", "¿Hola! Cómo estás?"];
-const languageDescriptions = ["It's my preferred medium of communication.", "Malayalam is my native language.", "I can speak a bit of Hindi.", "I've been learning Spanish for a year."];
 function About() {
-    const [index, setIndex] = useState(0);
     return (
         <div className='container about'>
             <div className='left'>
                 <InfoPane>
-                    <p>HELLO,<br />WORLD!</p>    
-                    <p>I'M ADERSH</p>   
+                    <p>HELLO,<br />WORLD!</p>
+                    <p>I'M ADERSH</p>
                 </InfoPane>
             </div>
             <div className='right'>
@@ -22,11 +17,6 @@ function About() {
                 <div className='about--text'>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ultricies metus eget dui tempor dictum sed a ante. Nullam gravida elit risus, ac pellentesque justo dapibus id. Suspendisse id pellentesque arcu. Cras facilisis non est dapibus sodales. Sed accumsan justo sit amet quam molestie, sed mollis dolor mattis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce tempus tincidunt quam, vitae commodo felis semper a. Praesent sed nibh tempus, aliquam turpis sit amet, vestibulum turpis. Sed tincidunt, libero non consectetur viverra, risus quam porttitor dui, sit amet bibendum odio erat quis dui. Nulla nec nulla risus. Fusce mattis erat vitae nisl dictum, nec lacinia lacus imperdiet. Quisque faucibus justo ac erat sagittis egestas. Praesent dapibus pulvinar pretium. Vivamus et nulla ut ante convallis molestie.</p>
                     <p>Nulla in felis semper, egestas lacus vel, venenatis ex. Pellentesque scelerisque orci nec odio finibus, in ullamcorper quam elementum. Nullam quis felis mollis, auctor velit at, rutrum quam. Morbi posuere id urna a vulputate. Quisque luctus tincidunt mattis. Vivamus sed dui eu neque tempor bibendum. Sed feugiat augue tristique felis luctus tincidunt. Aliquam lorem est, ornare vitae eleifend vel, porta et augue. Donec ornare posuere est ut interdum.</p>
-                    <h4>LANGUAGES</h4>
-                    <div className='about--text--language'>
-                        <Typewriter text={languageText} speed="50" callback={setIndex} />
-                        <FadeIn className="about--text--language__caption" key={languageDescriptions[index - 1]}>{languageDescriptions[index - 1]}</FadeIn>
-                    </div>
                 </div>
             </div>
         </div>
--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -1,50 +1,106 @@
-import React, { memo, useState } from 'react';
+import React, { memo, useState } from "react";
+import FadeIn from "react-fade-in";
+import Typewriter from "../Components/Typewriter";
 
 const skills = [
-    {
-        name: 'ReactJs',
-        category: 'Web Development',
-        logo: 'react.png'
-    },
-    {
-        name: 'JavaScript',
-        category: 'Web Development',
-        logo: 'javascript.png'
-    },
-    {
-        name: 'Java',
-        category: 'Backend',
-        logo: 'java.png'
-    },
-    {
-        name: 'Spring',
-        category: 'Backend',
-        logo: 'spring.png'
-    },
-    {
-        name: 'Python',
-        category: 'Backend / Scripting',
-        logo: 'python.png'
-    }
-]
-
+  {
+    name: "React",
+    category: "Web Development",
+    logo: "react.png",
+  },
+  {
+    name: "JavaScript",
+    category: "Web Development",
+    logo: "javascript.png",
+  },
+  {
+    name: "Java",
+    category: "Backend",
+    logo: "java.png",
+  },
+  {
+    name: "Spring",
+    category: "Backend",
+    logo: "spring.png",
+  },
+  {
+    name: "Python",
+    category: "Backend / Scripting",
+    logo: "python.png",
+  },
+  {
+    name: "MySQL",
+    category: "Database Management",
+    logo: "mysql.png",
+  },
+  {
+    name: "Docker",
+    category: "DevOps",
+    logo: "docker.png",
+  },
+  {
+    name: "AWS",
+    category: "DevOps",
+    logo: "aws.png",
+  },
+  {
+    name: "Agile",
+    category: "Software Development",
+    logo: "agile.png",
+  },
+];
+const language = {
+  languageText: [
+    "I am fluent in English.",
+    "നാട്ടിൽ എവിടെയാ ?",
+    "मैं कठिनाई से हिंदी बोल सकता हूं.",
+    "¿Hola! Cómo estás?",
+  ],
+  languageDescription: [
+    "It's my preferred medium of communication.",
+    "Malayalam is my native language.",
+    "I can speak a bit of Hindi.",
+    "I've been learning Spanish for a year.",
+  ],
+};
 function Skills() {
-    return (
-        <div className='skills'>
-            <div className='skills--header'>Skills</div>
-            <div className='skills--container'>
-                {skills.map(skill => (
-                    <div className='skill'>
-                        <div className='skill--logo'><img src={'./icons/' + skill.logo} alt={skill.name} /></div>
-                        <div className='skill--info'>
-                            <h4>{skill.category}</h4>
-                            <h3>{skill.name}</h3>
-                        </div>
-                    </div>
-                ))}
+  const [index, setIndex] = useState(0);
+  return (
+    <div className="container">
+      <div className="skills">
+        <h1 className="title">Skills</h1>
+        <div className="skills--container">
+          {skills.map((skill) => (
+            <div className="skill">
+              <div className="skill--logo">
+                <img src={"./icons/" + skill.logo} alt={skill.name} />
+              </div>
+              <div className="skill--info">
+                <h4>{skill.category}</h4>
+                <h3>{skill.name}</h3>
+              </div>
             </div>
+          ))}
         </div>
-    )
+      </div>
+      <div className="languages">
+        <h1 className="title">LANGUAGES</h1>
+        <div className="language">
+          <Typewriter
+            text={language.languageText}
+            speed="50"
+            callback={setIndex}
+          />
+          <FadeIn
+            className="language__caption"
+            key={language.languageDescription[index - 1]}
+          >
+            {language.languageDescription[index - 1]}
+          </FadeIn>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default memo(Skills);
