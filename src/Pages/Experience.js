6db8b84f2bf887340e02eee424461a94c453b33f
adershmanoj <adershmanoj@gmail.com>
2020-06-01 14:49:50 +0530

    Update portrait and experience tag

--- src/Pages/Experience.js
+++ src/Pages/Experience.js
@@ -10,7 +10,7 @@ const data = {
       description:
         "Working with the Cyber Security function to provide automation and software development capabilities." +
         "Successfully implemented Spam monitoring solution with sandboxed malware analysis.",
-      tags: ["Cyber Security", "Python", "Selenium", "JavaScript", "Mocha"],
+      tags: ["Cyber Security", "Python", "Selenium", "JavaScript", "Bash"],
     },
     {
       duration: "July 2018 - September 2018",
