cc4351b6cc68d9af8d87718ab5125d142c9ea0fd
adershmanoj <adershmanoj@gmail.com>
2020-06-09 19:44:37 +0530

    Styling changes for mobile responsiveness

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,40 +1,68 @@
-import React, { useState } from 'react';
-import Typewriter from '../Components/Typewriter';
-import FadeIn from 'react-fade-in';
-import Portrait from '../Components/Portrait';
-import Header from '../Components/Header';
+import React, { useState } from "react";
+import Typewriter from "../Components/Typewriter";
+import FadeIn from "react-fade-in";
+import Portrait from "../Components/Portrait";
+import Header from "../Components/Header";
 
 function Home() {
-    const interests = ["Coding", "Web Development", "Cyber Security", "Hackathons", "Reading", "Travelling", "...more"];
-    const typewriterSettings = {
-        speed: 75,
-        delay: 100
-    };
-    const [index, setIndex] = useState(0);
-    const returncurrentInterests = () => (
-        interests.slice(0, index).map(
-            (element, idx) => (idx === interests.length - 1 ?
-                <FadeIn key={element} className="fade code--red">{element}&nbsp;</FadeIn> :
-                <FadeIn key={element} className="fade code--red">{element},&nbsp;</FadeIn>
-            )
-        )
-    )
-    return (
-        <div className='container home'>
-            <div className='left'>
-                <Portrait />
+  const interests = [
+    "Coding",
+    "Web Development",
+    "Cyber Security",
+    "Hackathons",
+    "Reading",
+    "Travelling",
+    "...more",
+  ];
+  const typewriterSettings = {
+    speed: 75,
+    delay: 100,
+  };
+  const [index, setIndex] = useState(0);
+  const returncurrentInterests = () =>
+    interests.slice(0, index).map((element, idx) =>
+      idx === interests.length - 1 ? (
+        <FadeIn key={element} className="fade code--red">
+          {element}&nbsp;
+        </FadeIn>
+      ) : (
+        <FadeIn key={element} className="fade code--red">
+          {element},&nbsp;
+        </FadeIn>
+      )
+    );
+  return (
+    <div className="container home">
+      <div className="left">
+        <Portrait />
+      </div>
+      <div className="right">
+        {window.innerWidth > 600 && (
+          <Header first="ADERSH" last="MANOJ" height="500" />
+        )}
+        <div className="code">
+          <p>
+            <code className="code--darkblue">const</code>{" "}
+            <code className="code--lightblue">adersh</code>:{" "}
+            <code className="code--green">Array</code>&lt;
+            <code className="code--green">Interests</code>&gt;;
+          </p>
+          <div>
+            <code className="code--lightblue">adersh</code>.
+            <code className="code--yellow">push</code>(
+            <div className="code__typing"><Typewriter
+              callback={setIndex}
+              {...typewriterSettings}
+              className="code--red"
+              text={interests}
+            />
+            );
             </div>
-            <div className='right'>
-                <Header first="ADERSH" last="MANOJ" height="500" />
-                <div className='code'>
-                    <p><code className='code--darkblue'>const</code> <code className='code--lightblue'>adersh</code>: <code className='code--green'>Array</code>&lt;<code className='code--green'>Interests</code>&gt;;</p>
-                    <div><code className='code--lightblue'>adersh</code>.<code className='code--yellow'>push</code>(<Typewriter callback={setIndex}{...typewriterSettings} className='code--red' text={interests} />);</div>
-                    <div className='code__block'>
-                        [ {returncurrentInterests()}]
-                    </div>
-                </div>
-            </div>
+          </div>
+          <div className="code__block">[ {returncurrentInterests()}]</div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-export default React.memo(Home)
+export default React.memo(Home);
